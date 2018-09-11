348386188c608ee02e42a9e5fd414c41bbfdf19e
github username <github email address>
2018-09-11 09:15:48 -0400

    added static page styling

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :food_items
   resources :restaurants
   resources :users
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
