742da959ac9216556d882bb43fc075742e140bf5
Vladislav <vlads7539@gmail.com>
2022-05-03 20:22:40 +0300

    make it better

--- db/migrate/20180910140023_create_orders.rb
+++ db/migrate/20180910140023_create_orders.rb
@@ -3,7 +3,6 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.references :restaurant, foreign_key: true
       t.references :user, foreign_key: true
-      t.references :menuorder, foreign_key: true
 
       t.timestamps
     end
